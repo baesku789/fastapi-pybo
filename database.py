fa491af3b9065cc07b9d6556b82bc99fc9f979ff
baesku789 <39040522+baesku789@users.noreply.github.com>
2023-05-03 01:53:21 +0900

    .env 파일을 사용하도록 수정

--- database.py
+++ database.py
@@ -2,8 +2,10 @@ from sqlalchemy import MetaData
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from starlette.config import Config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+config = Config('.env')
+SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(url=SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
